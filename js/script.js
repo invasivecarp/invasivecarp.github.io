606f6ca6db084c57604850a6341228f95476b97d
invasivecarp <esanoluwamayowa@gmail.com>
2019-12-18 19:35:25 -0700

    hnnnngh

--- js/script.js
+++ js/script.js
@@ -2,3 +2,16 @@ document.querySelector('.continue').addEventListener('click', () => {
     document.getElementById('panel').classList.add('hidden');
     document.getElementById('app').classList.add('visible');
 });
+$(document).ready(function() {
+    Array.from(document.getElementById('navigation').children).forEach(element => {
+        element.addEventListener('click', function(e) {
+            console.log(e.currentTarget.getAttribute('chain'));
+            const f = e.currentTarget.getAttribute('chain');
+            //     console.log('#' + f);
+            //     $('html', 'body').animate({
+            //         scrollTop: $("#" + f).offset().top
+            //     }, 2000);
+            $('#' + f).velocity('scroll', { duration: 1000 })
+        })
+    });
+})
