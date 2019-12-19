983edf44241779206b15659e1b0c35441cf3ac67
invasivecarp <esanoluwamayowa@gmail.com>
2019-12-18 19:01:10 -0700

    fancy

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.querySelector('.continue').addEventListener('click', () => {
+    document.getElementById('panel').classList.add('hidden');
+    document.getElementById('app').classList.add('visible');
+});
