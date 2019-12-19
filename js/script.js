0c8966b5adfc6c2f3e0475e37b79193a320764ef
invasivecarp <esanoluwamayowa@gmail.com>
2019-12-18 22:27:56 -0700

    small fixes

--- js/script.js
+++ js/script.js
@@ -6,6 +6,8 @@ document.querySelector('.continue').addEventListener('click', () => {
 $(document).ready(function() {
     $('.trigger').click(e => {
         const f = e.currentTarget.getAttribute('chain');
+        document.querySelector('.active').classList.remove('active');
+        e.target.classList.add('active');
         $('html, body').animate({
             scrollTop: $('#' + f).offset().top + 'px'
         }, 1000);
