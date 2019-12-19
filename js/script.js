a033e0a93829edacaa3276a92eb59d7ca9a2de0c
invasivecarp <esanoluwamayowa@gmail.com>
2019-12-18 21:45:29 -0700

    fixed scrolling

--- js/script.js
+++ js/script.js
@@ -2,16 +2,12 @@ document.querySelector('.continue').addEventListener('click', () => {
     document.getElementById('panel').classList.add('hidden');
     document.getElementById('app').classList.add('visible');
 });
+
 $(document).ready(function() {
-    Array.from(document.getElementById('navigation').children).forEach(element => {
-        element.addEventListener('click', function(e) {
-            console.log(e.currentTarget.getAttribute('chain'));
-            const f = e.currentTarget.getAttribute('chain');
-            //     console.log('#' + f);
-            //     $('html', 'body').animate({
-            //         scrollTop: $("#" + f).offset().top
-            //     }, 2000);
-            $('#' + f).velocity('scroll', { duration: 1000 })
-        })
-    });
+    $('.trigger').click(e => {
+        const f = e.currentTarget.getAttribute('chain');
+        $('html, body').animate({
+            scrollTop: $('#' + f).offset().top + 'px'
+        }, 1000);
+    })
 })
